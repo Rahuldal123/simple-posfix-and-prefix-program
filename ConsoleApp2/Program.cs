79e0451fe98a09ea4bb739d0121ac3a31ebb9d45
Rahuldal123 <rahuldalwale55@gmail.com>
2022-09-14 11:16:15 +0530

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 12;
+            int n2 = ++n1;
+            int n3 = n2++;
+            Console.WriteLine($"n1 is{n1}, n2 is{n2},n3 is{n3}");
+
+
+        }
+    }
+}
